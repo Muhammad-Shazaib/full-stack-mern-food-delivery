32ffdc50b02ff2e24801d01c5a618a50a1641b5a
Muhammad Shazaib <shazaibirshad126@gmail.com>
2026-06-11 23:47:18 +0500

    Add package.json with initial dependencies for axios, react-router-dom, and react-toastify

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
-
+import React from "react";
+import Navbar from "./compenants/navbar/Navbar";
+import SideBar from "./compenants/sidebar/sideBar";
+import { Routes, Route } from "react-router-dom";
+import { Add } from "./pages/add/Add";
+import List from "./pages/list/List";
+import Order from "./pages/order/Order";
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <Navbar />
 
-export default App
+      <hr />
+      <div className="app-content">
+        <SideBar />
+        <Routes>
+          <Route path="/add" element={<Add />} />
+          <Route path="/list" element={<List />} />
+          <Route path="/orders" element={<Order />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
+
+export default App;
