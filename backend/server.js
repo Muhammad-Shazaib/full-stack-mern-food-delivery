4bb92470f6fa32bfb1518e3ad183ed354b6c02a9
Muhammad Shazaib <shazaibirshad126@gmail.com>
2026-06-16 23:37:53 +0500

    feat: implement user authentication with login and registration endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,17 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
 dotenv.config();
-import cors from 'cors';
-import { connectDB } from './config/db.js';
-import dns from "dns"
-import foodRouter from './routes/FoodRoutes.js';
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import dns from "dns";
+import foodRouter from "./routes/FoodRoutes.js";
+import userRouter from "./routes/UserRoutes.js";
 
-
-dns.setServers(["8.8.8.8","0.0.0.0"])
-
+dns.setServers(["8.8.8.8", "0.0.0.0"]);
 
 // app config
 const app = express();
-const port =  4000;
+const port = 4000;
 
 // middleware
 app.use(express.json());
@@ -23,14 +22,14 @@ connectDB();
 
 // api endpoints
 
-app.use('/api/food' , foodRouter);
-app.use('/images', express.static('uploads'));
+app.use("/api/food", foodRouter);
+app.use("/images", express.static("uploads"));
+app.use('/api/user', userRouter)
 
-app.get('/', (req, res) => {
-    res.send('ALLAH HO AKBAR!');
+app.get("/", (req, res) => {
+  res.send("ALLAH HO AKBAR!");
 });
 
 app.listen(port, () => {
-    console.log(`Server Started on http://localhost:${port}`);
+  console.log(`Server Started on http://localhost:${port}`);
 });
-
